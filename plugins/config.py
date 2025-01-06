b477542e0a76d4532849465dde4d4c30dba6b3ca
NYBotx <167092531+NYBotx@users.noreply.github.com>
2025-01-06 09:44:15 +0530

    Update config.py

--- plugins/config.py
+++ plugins/config.py
@@ -10,52 +10,50 @@ logging.basicConfig(
 )
 
 class Config(object):
-    # get a token from https://chatbase.com
-    CHAT_BASE_TOKEN = os.environ.get("CHAT_BASE_TOKEN", "")
-    # get a token from @BotFather
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    # The Telegram API things
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
-    # Get these values from my.telegram.org
-    # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
-    # Banned Unwanted Members..
-    BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
-    # the download location, where the HTTP Server runs
-    DOWNLOAD_LOCATION = "./DOWNLOADS"
-    COOKIES_LOCATION = "./COOKIES"
-    # Update channel for Force Subscribe
-    UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "")
-    # Telegram maximum file upload size
-    MAX_FILE_SIZE = 50000000
-    TG_MAX_FILE_SIZE = 2097152000
-    FREE_USER_MAX_FILE_SIZE = 50000000
-    # chunk size that should be used with requests
-    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
-    # default thumbnail to be used in the videos
-    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
-    # proxy for accessing youtube-dl in GeoRestricted Areas
-    # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
-    HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
-    TAOBAO_PROXY = os.environ.get("TAOBAO_PROXY", "")
-    # https://t.me/hevcbay/951
-    OUO_IO_API_KEY = ""
-    # maximum message length in Telegram
-    MAX_MESSAGE_LENGTH = 4096
-    # set timeout for subprocess
-    PROCESS_MAX_TIMEOUT = 3600
-    # watermark file
-    DEF_WATER_MARK_FILE = ""
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
-    SESSION_NAME = os.environ.get("SESSION_NAME", "Rename-Bot-0")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", -100))
-    LOGGER = logging
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-    # Update channel for Force Subscribe
-    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8120897740:AAHBzDf484aQZwOlz4Sp5s7rwfHAMyU0JNM")
     
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "TellyUploaderBot")
+    API_ID = int(os.environ.get("API_ID", "13963336"))
+    
+    API_HASH = os.environ.get("API_HASH", "a144d1e22ef0b29738e8c00713d02678")
+    
+    DOWNLOAD_LOCATION = "./DOWNLOADS"
+    
+    MAX_FILE_SIZE = 2097152000
+    
+    TG_MAX_FILE_SIZE = 2097152000
+    
+    FREE_USER_MAX_FILE_SIZE = 2097152000
+    
+    CHUNK_SIZE = int(os.environ.get("CHUNK_SIZE", 128))
+    
+    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "")
+    
+    HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
+    
+    OUO_IO_API_KEY = ""
+    
+    MAX_MESSAGE_LENGTH = 4096
+    
+    PROCESS_MAX_TIMEOUT = 0
+    
+    DEF_WATER_MARK_FILE = "Url_uploader_nybot"
+    
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Nischay999:Nischay999@cluster0.5kufo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    
+    SESSION_NAME = os.environ.get("SESSION_NAME", "Url_uploader_nybot")
+    
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002421238378"))
+    
+    LOGGER = logging
+
+    UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002465691872")
+    
+    OWNER_ID = int(os.environ.get("OWNER_ID", "2103299862"))
+    
+    TG_MIN_FILE_SIZE = 2097152000
+    
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Url_uploader_nybot")
+                                  
     PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
     PRO_USERS.append(OWNER_ID)
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
